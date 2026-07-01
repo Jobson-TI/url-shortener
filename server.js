adc22fa06d49ed5096abbab192ca734bd4a544c6
Jobson-TI <jobsonnazaredorosario@gmail.com>
2026-07-01 20:08:15 -0300

    adiciona autenticacao

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const { Pool } = require('pg')
+const express = require('express')//importado o express que foi instalador
+const { Pool } = require('pg')//importado o banco de dados
 const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+const jwt = require('jsonwebtoken')//token de acesso importado
+const JWT_SECRET = process.env.JWT_SECRET || 'chave-secreta-local' 
+const bcrypt = require('bcrypt')
 
 async function initDB() {
+  //tabela do encurtador
   await pool.query(`
     CREATE TABLE IF NOT EXISTS urls (
       slug TEXT PRIMARY KEY,
@@ -11,19 +15,29 @@ async function initDB() {
       ultimoAcesso TEXT
     )
   `)
+  //tabela de usuario
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id SERIAL PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        senha TEXT NOT NULL
+    )
+ `)
+ await pool.query(`ALTER TABLE urls ADD COLUMN IF NOT EXISTS usuario_id INTEGER REFERENCES usuarios(id)`)
 }
 initDB()
  const { nanoid } = require('nanoid')
 
  const app = express()
- app.use(express.json())
- app.use(express.static('public'))
+ app.use(express.json())//transformar os textos em json
+ app.use(express.static('public'))//usado esse script na pasta public
 
  app.get('/teste', (req, res) => {
     res.send('API funcionado!')
  })
 
- app.post('/api/encurtar', async (req, res) => {
+ //o encurtado do site aqui ele transformar o link em curto
+ app.post('/api/encurtar', autenticar, async (req, res) => {
     const { url } = req.body
     const base = `${req.protocol}://${req.get('host')}`
 
@@ -38,7 +52,7 @@ initDB()
     }
 
     const slug = nanoid(6)
-    await pool.query('INSERT INTO urls (slug, original, clicks, ultimoAcesso) VALUES ($1, $2, $3, $4)', [slug, url, 0, new Date().toISOString()])
+    await pool.query('INSERT INTO urls (slug, original, clicks, ultimoAcesso, usuario_id) VALUES ($1, $2, $3, $4, $5)', [slug, url, 0, new Date().toISOString(), req.usuarioId])
 
     res.status(201).json({
         slug,
@@ -47,16 +61,66 @@ initDB()
     })
 })
 
- app.get('/api/urls', async (req, res) => {
-    const result = await pool.query('SELECT * FROM urls')
-    res.json(result.rows)
+//cadastro
+ app.post('/api/cadastro', async (req, res) => {
+    const { email, senha } = req.body
+
+    if (!email || !senha) {
+        return res.status(400).json({ erro: 'Email e senha obrigatórios'})
+    }
+
+    const hash = await bcrypt.hash(senha, 10)
+    await pool.query('INSERT INTO usuarios (email, senha) VALUES ($1, $2)', [email, hash])
+    res.status(201).json({ mensagem: 'Usuário criado com sucesso'})
 })
 
- app.delete('/api/urls/:slug', async (req, res) => {
+//verificado de token
+ function autenticar(req, res, next) {
+    const token = req.headers.authorization?.replace('Bearer ', '')
+
+    if (!token) {
+        return res.status(401).json({ erro: 'Token não fornecido' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET)
+        req.usuarioId = decoded.id
+        next()   
+    } catch {
+        return res.status(401).json({ erro: 'Token inválido' })
+    }
+ }
+
+//login
+ app.post('/api/login', async (req, res) => {
+    const { email, senha } = req.body
+    const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email])
+    const usuario = result.rows[0]
+
+    if (!usuario) {
+        return res.status(400).json({ erro: 'Email não cadastrado'})
+    }
+
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha)
+
+    if (!senhaCorreta) {
+        return res.status(400).json({ erro: 'Senha incorreta'})
+    }
+
+    const token = jwt.sign({ id: usuario.id }, JWT_SECRET, {expiresIn: '7d'})
+    res.json({ token })
+ })
+//mostrar todas as urls
+ app.get('/api/urls', autenticar, async (req, res) => {
+    const result = await pool.query('SELECT * FROM urls WHERE usuario_id = $1', [req.usuarioId])
+    res.json(result.rows)
+})
+//deletar as urls
+ app.delete('/api/urls/:slug', autenticar, async (req, res) => {
     await pool.query('DELETE FROM urls WHERE slug = $1', [req.params.slug])
     res.json({ mensagem: `URL ${req.params.slug} deletada com sucesso` })
 })
-
+//temporizado para deletar url depois de um tempo
  setInterval(async () => {
     const result = await pool.query('SELECT * FROM urls')
     const agora = new Date()
@@ -69,6 +133,7 @@ initDB()
     })
 }, 60000)
 
+//o redirecionador do encurtador para o site real
     app.get('/:slug', async (req, res) => {
         const result = await pool.query('SELECT * FROM urls WHERE slug = $1', [req.params.slug])
         const url = result.rows[0]
